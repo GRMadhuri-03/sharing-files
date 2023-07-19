0c691fb5ced0988e53397c81828fb698b4d74193
GRMadhuri-03 <124073241+GRMadhuri-03@users.noreply.github.com>
2023-07-19 11:42:00 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -7,7 +7,7 @@ const DBConnection = async () => {
     const USERNAME = process.env.DB_USERNAME;
     const PASSWORD = process.env.DB_PASSWORD;
 
-    const MONGO_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.arkk6ci.mongodb.net/?retryWrites=true&w=majority`;
+   const MONGO_URI= `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.w6jyhsx.mongodb.net/?retryWrites=true&w=majority`;
     try {
         await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
         console.log('Database connected successfully');
